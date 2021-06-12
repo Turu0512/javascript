6c8f4f810fcc1f6887ed73e8f9f8cf63729ea82a
Turu0512 <hinataptyan@gmail.com>
2021-06-13 08:50:13 +0900

    要素の複製　挿入　clone insert 使い方

--- main.js
+++ main.js
@@ -4,10 +4,11 @@
 
 
 document.querySelector(`button`).addEventListener(`click`,()=>{
-  const item2 = document.createElement(`li`);
-  item2.textContent = `item 2`;
-  // const ulNode = document.querySelector(`ul`);
-  const ul = document.querySelector(`ul`);
-  ul.appendChild(item2);
+ const item0 = document.querySelectorAll(`li`)[0];
+ const copy = item0.cloneNode(true);
+
+ const ul = document.querySelector(`ul`)
+ const item2 = document.querySelectorAll(`li`)[2];
+ ul.insertBefore(copy,item2);
 });
 }
