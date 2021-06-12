a7635edd786cd987511b817062b3d04b0183d7b3
Turu0512 <hinataptyan@gmail.com>
2021-06-13 08:24:21 +0900

    要素の追加

--- main.js
+++ main.js
@@ -4,9 +4,10 @@
 
 
 document.querySelector(`button`).addEventListener(`click`,()=>{
-  const targetNode = document.getElementById('target')
-// targetNode.textContent="Dotinstall";
-targetNode.textContent=targetNode.dataset.translation;
-
+  const item2 = document.createElement(`li`);
+  item2.textContent = `item 2`;
+  // const ulNode = document.querySelector(`ul`);
+  const ul = document.querySelector(`ul`);
+  ul.appendChild(item2);
 });
 }
